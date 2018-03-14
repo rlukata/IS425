53375fee2d00c3c986a3290ee80706f6ae5f2eae
Rami Lukata <rlukata@cityuniversity.edu>
2018-03-13 17:48:17 -0700

    Lab 7, 8

--- RandomFacts/RandomFacts/MainWindow.xaml.cs
+++ RandomFacts/RandomFacts/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using System.Windows;
 
 namespace RandomFacts
@@ -15,19 +15,19 @@ namespace RandomFacts
         /**
          * Variable declaration
          */
-        MyFunctions myFunction = new MyFunctions();
-        List<string> myContent;
+        private MyFunctions myFunction = new MyFunctions();
+        private List<string> myContent;
 
         //static variable that holds the path of the initial data
-        internal static string myInitFile = @"randomFacts.txt";
+        internal static string myInitFile = "randomFacts.txt";
 
         // static variable that holds the path of edited files
-        internal static string myTempFile = @"editedFacts.txt";
+        internal static string myTempFile = "editedFacts.txt";
 
         //holder that contains error message info for try catch block across all classes
-        internal static List<string> holder = 
+        internal static List<string> holder =
             new List<string> { "something went wrong", "load another text file", "the file is empty" };
-        
+
         public MainWindow()
         {
             InitializeComponent();
@@ -37,11 +37,12 @@ namespace RandomFacts
         }
 
         /**
-         * Event that triggers the click event on the button randomFactButton1
+         * Event that triggers the click event on the button randomFactButton
          * This allows the program to load a random fact to begin with
-         * It's enable or disable, add/remove Loaded="Window_Loaded" to/from the Window 
+         * It's enable or disable, add/remove Loaded="Window_Loaded" to/from the Window
          * element in XAML
          */
+
         public void Window_Loaded(object sender, RoutedEventArgs e)
         {
             try
@@ -57,8 +58,9 @@ namespace RandomFacts
         }
 
         /**
-         * Events triggered by buttons 
+         * Events triggered by buttons
          */
+
         //open button
         private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
         {
@@ -84,26 +86,34 @@ namespace RandomFacts
         {
             randomFactTextBlock.Text = myFunction.LoadContentIntoFunction(myContent);
         }
-        
+
         //edit button
         private void MenuItemEdit_Click(object sender, RoutedEventArgs e)
         {
             myContent = myFunction.SaveAndEditContentIntoFile(myContent);
             this.MenuItemClickMe_Click(sender, e);
         }
-    }
 
+        //open non-rectangular window
+        private void MenuItemNonRec_Click(object sender, RoutedEventArgs e)
+        {
+            Non_rect non_Rect = new Non_rect();
+            non_Rect.Show();
+        }
+    }
 
     /**
      * Partial class that creates several public functions for the
      * GUI to use and pass data around
      */
+
     partial class MyFunctions
     {
         /**
          * The function stores the data in an array of strings
          * it returns the array of data to be used by the program
          */
+
         internal List<string> SaveContentFromData(string myData)
         {
             List<string> lines = new List<string> { };
@@ -115,6 +125,7 @@ namespace RandomFacts
          * The function takes a list string of data, stores it in a new file, then it calls
          * the EditContentOfFile function to open Notepad and edit contents
          */
+
         internal List<string> SaveAndEditContentIntoFile(List<string> myData)
         {
             File.WriteAllLines(MainWindow.myTempFile, myData);
@@ -123,24 +134,28 @@ namespace RandomFacts
         }
 
         /**
-         * Process that calls process class to start and close notepad on .txt files 
+         * Process that calls process class to start and close notepad on .txt files
          */
-        void EditContentOfFile(string myPath)
+
+        private void EditContentOfFile(string myPath)
         {
-            var process = new Process();
-            process.StartInfo = new ProcessStartInfo()
+            var process = new Process
             {
-                UseShellExecute = true,
-                FileName = myPath
+                StartInfo = new ProcessStartInfo()
+                {
+                    UseShellExecute = true,
+                    FileName = myPath
+                }
             };
             process.Start();
             process.WaitForExit();
         }
-        
+
         /**
          * This function also keeps sorting through the array if it finds an empty string
          * it returns the string to be used by the GUI
          */
+
         internal string LoadContentIntoFunction(List<string> myContent)
         {
             Random rand = new Random();
@@ -153,22 +168,25 @@ namespace RandomFacts
          * Function that opens a specific type of file
          * it's designed to only find and look for .txt files
          */
+
         internal List<string> OpenTxtFile()
         {
             //Create string variable to return
             List<string> myContent = MainWindow.holder;
 
-            // Create OpenFileDialog 
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
 
-            // Set filter for file extension and default file extension 
-            dlg.DefaultExt = ".txt";
-            dlg.Filter = "TXT Files (*.txt)|*.txt";
+                // Set filter for file extension and default file extension
+                DefaultExt = ".txt",
+                Filter = "TXT Files (*.txt)|*.txt"
+            };
 
-            // Display OpenFileDialog by calling ShowDialog method 
+            // Display OpenFileDialog by calling ShowDialog method
             Nullable<bool> result = dlg.ShowDialog();
 
-            // Get the selected file name and display in a TextBox 
+            // Get the selected file name and display in a TextBox
             if (result == true)
             {
                 // Open and read from document
@@ -184,10 +202,11 @@ namespace RandomFacts
                 {
                     throw new IndexOutOfRangeException();
                 }
-            }catch
+            }
+            catch
             {
                 myContent = MainWindow.holder;
-            }            
+            }
             return myContent;
         }
     }
---
+++ RandomFacts/RandomFacts/Non-rect.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace RandomFacts
+{
+    /// <summary>
+    /// Interaction logic for Non_rect.xaml
+    /// </summary>
+    public partial class Non_rect : Window
+    {
+        public Non_rect()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
